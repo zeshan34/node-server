2515febecda4ae663da66f33dd81b0b1eea56cb1
Muhammad Zeshan Shaikh <shaikhzeshan@gmail.com>
2020-08-08 12:51:23 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
-const http = require('http');
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
+
 
 const hostname ='localhost';
-
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    console.log(req.headers);
-    res.statusCode = '200';
-    res.setHeader('Content-Type','text/html');
-    res.end('<html><body><h1>hello world!</h1></body></html>');
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
+
+
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req,res) =>{
+    res.statusCode = 200;
+    res.setHeader('Content-type','text/html');
+    res.end('<html><body><h1>this is an Express Server</h1></body></html>');
+
 
 });
-
-server.listen(port, hostname, () =>{
-    console.log(`Server running at http://${hostname}:${port}`)
+app.listen(port, hostname, () =>{
+    console.log(`server runing at http://${hostname}:${port}`)
 });
